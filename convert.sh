787985f4dacd5f5bc3900771c15789442c4a0074
Asher Glick <asher.glick@gmail.com>
2012-10-23 02:52:02 -0400

    updated some images and created a footer and wrapper to allways have a scroll bar on each page

--- convert.sh
+++ convert.sh
@@ -1,2 +1,5 @@
-../amassite.py amassite/index.html htdocs/index.html -C
-../amassite.py amassite/projects.html htdocs/projects.html -C
+# Eventually amassite will be an installable program, but as of now it is not
+../Amassite/amassite.py amassite/index.html htdocs/index.html -C
+../Amassite/amassite.py amassite/projects.html htdocs/projects.html -C
+../Amassite/amassite.py amassite/downloads.html htdocs/download.html -C
+../Amassite/amassite.py amassite/charliecube.html htdocs/charliecube.html -C
