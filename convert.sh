1eeb41d7d6935728914df81b3601c10856b4fed0
Asher Glick <asher.glick@gmail.com>
2013-02-05 11:52:45 -0500

    updated the convert script for the new amassite commands

--- convert.sh
+++ convert.sh
@@ -1,5 +1,3 @@
 # Eventually amassite will be an installable program, but as of now it is not
-../Amassite/amassite.py amassite/index.html htdocs/index.html -C
-../Amassite/amassite.py amassite/projects.html htdocs/projects.html -C
-../Amassite/amassite.py amassite/downloads.html htdocs/downloads.html -C
-../Amassite/amassite.py amassite/charliecube.html htdocs/charliecube.html -C
+rm -r htdocs/*
+../Amassite/amassite.py amassite htdocs
